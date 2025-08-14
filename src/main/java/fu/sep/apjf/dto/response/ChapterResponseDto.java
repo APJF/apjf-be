c3903ce3ba1b0e60b3e101298b11cf0494078ce4
dat369 <116793130+dat369@users.noreply.github.com>
2025-08-14 08:39:37 +0700

    modified clean code

--- src/main/java/fu/sep/apjf/dto/response/ChapterResponseDto.java
+++ src/main/java/fu/sep/apjf/dto/response/ChapterResponseDto.java
@@ -2,11 +2,6 @@ package fu.sep.apjf.dto.response;
 
 import fu.sep.apjf.entity.EnumClass;
 
-import java.util.Set;
-
-/**
- * DTO đại diện cho phản hồi thông tin chương
- */
 public record ChapterResponseDto(
         String id,
         String title,
