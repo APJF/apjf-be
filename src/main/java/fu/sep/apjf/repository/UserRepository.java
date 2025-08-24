41092e5e9abdf7071d9a648fe6c016b4a92402de
dat369 <116793130+dat369@users.noreply.github.com>
2025-08-24 14:24:42 +0700

    add createAt in user

--- src/main/java/fu/sep/apjf/repository/UserRepository.java
+++ src/main/java/fu/sep/apjf/repository/UserRepository.java
@@ -4,6 +4,7 @@ import fu.sep.apjf.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmailIgnoreCase(String email);
 
     Optional<User> findByEmailIgnoreCase(String email);
+
+    List<User> findAll();
 }
