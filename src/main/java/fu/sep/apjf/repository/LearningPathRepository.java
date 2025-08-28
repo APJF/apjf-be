78cd54f205e7a93817aafb02e2446ff5705aa290
dat369 <116793130+dat369@users.noreply.github.com>
2025-08-28 23:00:47 +0700

    fix lazy session learning path

--- src/main/java/fu/sep/apjf/repository/LearningPathRepository.java
+++ src/main/java/fu/sep/apjf/repository/LearningPathRepository.java
@@ -6,12 +6,20 @@ import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface LearningPathRepository extends JpaRepository<LearningPath, Long> {
-    List<LearningPath> findByUserId(Long userId);
+    @Query("""
+        SELECT lp FROM LearningPath lp
+        LEFT JOIN FETCH lp.courseLearningPaths clp
+        LEFT JOIN FETCH clp.course
+        WHERE lp.user.id = :userId
+    """)
+    List<LearningPath> findByUserIdWithCourses(@Param("userId") Long userId);
+
 
     Optional<LearningPath> findByUserIdAndStatus(Long userId, EnumClass.PathStatus status);
 
